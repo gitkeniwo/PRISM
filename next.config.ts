6f5261e7be4188b8356401597bb167aaebdfa88f
gitkeniwo <kennethwwwww@gmail.com>
2025-12-06 15:46:05 +0100

    test(ci): enable workflow on test branch

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: "/PRISM",
+  assetPrefix: "/PRISM",
   images: {
     unoptimized: true,
   },
